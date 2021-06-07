315ad4b506f16823bfd68c493214f1c444168bf0
skoduru001 <satyasaikrishna23@gmail.com>
2021-06-06 23:16:58 -0400

    day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
+  styles:['' +
+  'h3{' +
+  'color: blue' +
+  '}'
+  ]
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
